1f75553d8440ade0e24fae5ca40d546720ee194b
saipranav333444 <suryasaipranav@gmail.com>
2024-08-07 06:16:37 +0000

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+import UserInfo from '../UserInfo'
+import BlogList from '../BlogList'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
